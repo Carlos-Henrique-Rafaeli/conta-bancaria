9865dc933012cb2ce160573c0a00331e7540a4ee
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-04-02 15:12:35 -0300

    Implementado todas as operações bancarias na classe ContaCorrente

--- ContaBancaria.ConsoleApp/ContaCorrente.cs
+++ ContaBancaria.ConsoleApp/ContaCorrente.cs
@@ -5,7 +5,7 @@ public class ContaCorrente
     public double saldo;
     public double limite;
     public int numero;
-    
+
     public ContaCorrente(double saldo, double limite, int numero)
     {
         this.saldo = saldo;
@@ -13,17 +13,38 @@ public class ContaCorrente
         this.numero = numero;
     }
 
-    public void Sacar()
+    public void Sacar(double valor)
     {
-
+        double limitePermitido = saldo + limite;
+        
+        if (valor > 0)
+        {
+            if (valor <= limitePermitido)
+                saldo -= valor;
+        }
     }
 
-    public void Depositar()
+    public void Depositar(double valor)
     {
-
+        if (valor > 0)
+            saldo += valor;
     }
 
-    public void TransferirPara()
+    public void TransferirPara(ContaCorrente contaParaTransferir, double valor)
+    {
+        double limitePermitido = saldo + limite;
+
+        if (valor > 0)
+        {
+            if (valor <= limitePermitido)
+            {
+                contaParaTransferir.Depositar(valor);
+                saldo -= valor;
+            }
+        }
+    }
+
+    public void ExibirExtrato()
     {
 
     }
--- ContaBancaria.ConsoleApp/Program.cs
+++ ContaBancaria.ConsoleApp/Program.cs
@@ -4,8 +4,22 @@ internal class Program
 {
     static void Main(string[] args)
     {
+        ContaCorrente conta1 = new ContaCorrente(1000.0, 0.0, 12);
+        ContaCorrente conta2 = new ContaCorrente(300.0, 0.0, 13);
 
+        Console.WriteLine($"{conta1.saldo} {conta2.saldo}");
 
+        conta1.Sacar(200.0);
+        
+        conta1.Depositar(300.0);
+        
+        conta1.Depositar(500.0);
+        
+        conta1.Sacar(200.0);
+
+        conta1.TransferirPara(conta2, 400.0);
+
+        Console.WriteLine($"{conta1.saldo} {conta2.saldo}");
 
         Console.ReadLine();
     }
